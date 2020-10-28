e8608195a93a3d3b632ba3d6c7e9b046a857099a
lincey-cherian-au7 <linceycherian@gmail.com>
2020-10-28 12:36:28 +0530

    added reducers

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import store from './store/store';
+
+const configurestore = store();
 
 
 
 ReactDOM.render(
-<Provider>
+<Provider store={configurestore}>
     <App />
 </Provider>, document.getElementById('root'));
 
